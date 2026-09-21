b2961442ad1dcf27e5ff99adbddde496d0292e57
keisuke-k28 <sinano.k.k.dora828@gmail.com>
2026-09-22 02:16:33 +0900

    Integrate electricity demand prediction feature
    Added functionality to predict electricity demand using forecast data.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from historical_weather import load_historical_weather
 from electricity import load_electricity_demand
 from model import train_model
 from plot import create_plot
+from prediction import predict_electricity_demand
 
 
 def main():
@@ -30,3 +31,44 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    # JSONから取得した予測気温データ
+    df_forecast = pd.DataFrame({
+
+        "都道府県": [
+            "茨城県",
+            "栃木県",
+            "群馬県",
+            "埼玉県",
+            "千葉県",
+            "東京都",
+            "神奈川県"
+        ],
+
+        "予測気温": [
+            25.0,
+            24.5,
+            27.0,
+            26.0,
+            25.5,
+            25.0,
+            24.5
+        ],
+
+        "人口": [
+            2810000,
+            1900000,
+            1900000,
+            7300000,
+            6300000,
+            14100000,
+            9200000
+        ]
+
+    })
+
+    # 電力需要を予測
+    df_prediction = predict_electricity_demand(
+        model,
+        df_forecast
+    )
